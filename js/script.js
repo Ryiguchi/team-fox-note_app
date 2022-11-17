b146051a857d4c95e77b0b3c84dc7fa9a16284de
Ryan Iguchi <ryan.iguchi1@gmail.com>
2022-11-17 22:18:52 +0100

    added my branch

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
 "use strict";
-
-// Selectors//
-// -side-bar nav
-const iconSearchNav = document.querySelector(".icon-search");
-const iconStarNav = document.querySelector(".icon-search");
-const iconTagNav = document.querySelector(".icon-search");
