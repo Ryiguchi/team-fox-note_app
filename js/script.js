0a1d9a2f4306b2abbcbc90c780662be481f14417
Ryan Iguchi <ryan.iguchi1@gmail.com>
2022-11-17 22:13:08 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Selectors//
+// -side-bar nav
+const iconSearchNav = document.querySelector(".icon-search");
+const iconStarNav = document.querySelector(".icon-search");
+const iconTagNav = document.querySelector(".icon-search");
