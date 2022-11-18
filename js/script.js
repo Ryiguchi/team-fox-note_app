0ec6b385f3cdd96bab349ea1b703b311d830cda9
Ryan Iguchi <ryan.iguchi1@gmail.com>
2022-11-18 21:40:19 +0100

    added save and new note feature

--- js/script.js
+++ js/script.js
@@ -22,12 +22,26 @@ let quill = new Quill("#note-textarea", {
 // SELECTORS
 const toolbar = document.querySelector(".ql-toolbar");
 const btnCloseWelcome = document.querySelector(".welcome-close-btn");
+const btnNewNote = document.querySelector(".icon-plus");
 const welcomePopUp = document.querySelector(".welcome-pop-up");
+const saveBtn = document.querySelector(".icon-save");
 const noteTextarea = document.querySelector("#note-textarea");
 
 // State = data representing the current state of the app
-const state = {
+let state = {
+  curNote: {
+    tags: [],
+    id: null,
+  },
   firstLogIn: true,
+  savedNotes: [],
+};
+let listen = true;
+
+// FUNCTIONS ///////////////////////////
+
+const wait = function (handler, sec) {
+  setTimeout(handler, sec * 1000);
 };
 
 /**
@@ -57,18 +71,70 @@ const toggleWelcome = function () {
   toolbar.classList.toggle("hidden");
 };
 
+const saveNote = function () {
+  const delta = quill.getContents();
+  const index = state.savedNotes.findIndex(
+    (note) => note.id === state.curNote.id
+  );
+
+  index === -1 ? saveNewNote(delta) : updateNote(delta, index);
+};
+
+const updateNote = function (delta, i) {
+  state.savedNotes[i].delta = delta;
+  setLocalStorage(state);
+};
+
+const saveNewNote = function (delta) {
+  state.curNote.date = getDate();
+  state.curNote.id = createID();
+  state.curNote.delta = delta;
+  createSavedNote();
+  setLocalStorage(state);
+};
+
+// create a deep copy of the current Note
+const createSavedNote = function () {
+  const tempNote = {
+    date: state.curNote.date,
+    id: state.curNote.id,
+    delta: state.curNote.delta,
+    tags: [...state.curNote.tags],
+  };
+  state.savedNotes.push(tempNote);
+};
+
+const getDate = function () {
+  return new Date().toString().slice(4, 15);
+};
+
+const createID = function () {
+  return Date.now().toString().slice(5);
+};
+
+const clearCurID = function () {
+  state.curNote.id = createID();
+};
+
+const updateState = function (data) {
+  const { firstLogIn, savedNotes } = data;
+  state.firstLogIn = firstLogIn;
+  state.savedNotes = [...savedNotes];
+};
+
 /**
  * Anything in here will be executed when the page loads
  * @author Ryan Iguchi
  */
 const init = function () {
   const data = getLocalStorage();
-  if (data === null || data.firstLogIn === true) toggleWelcome();
+  updateState(data);
+  if (data === null || state.firstLogIn === true) toggleWelcome();
 };
 
 init();
 
-// EVENT HANDLERS
+// EVENT HANDLERS //////////////////////////
 
 btnCloseWelcome.addEventListener("click", (e) => {
   state.firstLogIn = false;
@@ -76,6 +142,15 @@ btnCloseWelcome.addEventListener("click", (e) => {
   toggleWelcome();
 });
 
-/* As a user, I want to be able to create headings, bulleted lists, numbered lists and make text italic or bold
- *  @AUTHOr Revan Toma
- */
+saveBtn.addEventListener("click", saveNote);
+
+// quill.on("text-change", () => {
+//   if(!listen) return
+//   wait(saveNote, 2);
+//   listen = false;
+// });
+
+btnNewNote.addEventListener("click", () => {
+  clearCurID();
+  quill.setContents([{ insert: "\n" }]);
+});
