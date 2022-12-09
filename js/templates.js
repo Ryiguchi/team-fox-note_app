f3de3da607d6a9650afba428ecf9125b9e123b15
Ryan Iguchi <ryan.iguchi1@gmail.com>
2022-12-09 23:17:53 +0100

    added js-file for template-delta

---
+++ js/templates.js
@@ -0,0 +1,979 @@
+export const recipeTemplate = [
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert: "RECIPE",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      underline: true,
+      background: "transparent",
+      bold: true,
+    },
+    insert: '"Put the name of the recipe here"',
+  },
+  {
+    insert: "\n\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert: "Ready in ",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#000000",
+      bold: true,
+    },
+    insert: "20 minutes",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert: "Serves ",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#000000",
+      bold: true,
+    },
+    insert: "8 people",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#000000",
+      bold: true,
+    },
+    insert: "280 calories",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert: " ",
+  },
+  {
+    insert: "\n\n\nIngrediens",
+  },
+  {
+    attributes: {
+      header: 1,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Lorem ipsum dolor sit amet",
+  },
+  {
+    attributes: {
+      list: "bullet",
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Consectetuer adipiscing elit",
+  },
+  {
+    attributes: {
+      list: "bullet",
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Suspendisse scelerisque",
+  },
+  {
+    attributes: {
+      list: "bullet",
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Libero interdum auctor",
+  },
+  {
+    attributes: {
+      list: "bullet",
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Preparation",
+  },
+  {
+    attributes: {
+      header: 1,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#000000",
+      bold: true,
+    },
+    insert: "Lorem ipsum dolor sit amet",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert:
+      " consectetuer adipiscing elit sed do tempor incididunt ut labore et dolore magna aliqua. ",
+  },
+  {
+    attributes: {
+      list: "ordered",
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+  },
+  {
+    attributes: {
+      list: "ordered",
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#000000",
+      bold: true,
+    },
+    insert: "Suspendisse scelerisque mi a mi.",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert:
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed dolore eiusmod tempor.",
+  },
+  {
+    attributes: {
+      list: "ordered",
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#000000",
+      bold: true,
+    },
+    insert: "Vestibulum ante ipsum primis elementum",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert: ", libero interdum auctor cursus, sapien enim dictum quam.",
+  },
+  {
+    attributes: {
+      list: "ordered",
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert:
+      "Phasellus vehicula nonummy nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim ad minim veniam, quis nostrud exercitation.",
+  },
+  {
+    attributes: {
+      list: "ordered",
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert: "Ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    attributes: {
+      list: "ordered",
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Tips",
+  },
+  {
+    attributes: {
+      header: 1,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert:
+      "Lorem ipsum dolor sit amet consectetuer adipiscing elit sed do tempor incididunt ut labore et dolore magna aliqua. ",
+  },
+  {
+    insert: "\n\n\n",
+  },
+];
+
+export const resumeTemplate = [
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert: "Resume",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      bold: true,
+    },
+    insert: "Your name",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Insert text here Insert text here Insert text here ",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      italic: true,
+    },
+    insert: "Storgatan 1",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      italic: true,
+    },
+    insert: "Storstad 123 45",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      // background: "#ffffff",
+      bold: true,
+    },
+    insert: "(+46) 000-00 00 00",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      bold: true,
+    },
+    insert: "no_reply@example.com",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    insert: "\n\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#000000",
+    },
+    insert: "EXPERIENCE",
+  },
+
+  {
+    attributes: {
+      header: 1,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Company, Location –  ",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      italic: true,
+    },
+    insert: "MONTH",
+  },
+  {
+    attributes: {
+      header: 2,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert: "20XX – NOW",
+  },
+  {
+    attributes: {
+      header: 3,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert:
+      "Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here.",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Company, Location -  ",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      italic: true,
+    },
+    insert: "MONTH",
+  },
+  {
+    attributes: {
+      header: 2,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert: "20XX - MONTH 20XX",
+  },
+  {
+    attributes: {
+      header: 3,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert:
+      "Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here.",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Company, Location -  ",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      italic: true,
+    },
+    insert: "MONTH",
+  },
+  {
+    attributes: {
+      header: 2,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert: "20XX - MONTH 20XX",
+  },
+  {
+    attributes: {
+      header: 3,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert:
+      "Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here.",
+  },
+  {
+    insert: "\n\n",
+  },
+  {
+    insert: "\n\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#000000",
+    },
+    insert: "EDUCATION",
+  },
+  {
+    attributes: {
+      header: 1,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "School name, Place - ",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      italic: true,
+    },
+    insert: "Degree",
+  },
+  {
+    attributes: {
+      header: 2,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert: "MONTH 20XX - MONTH 20XX",
+  },
+  {
+    attributes: {
+      header: 3,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert:
+      "Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here Insert text here.",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "School Name, Place - ",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      italic: true,
+    },
+    insert: "Degree",
+  },
+  {
+    attributes: {
+      header: 2,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert: "MONTH 20XX - MONTH 20XX",
+  },
+  {
+    attributes: {
+      header: 3,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert:
+      "Insert text here Insert text here Insert text here Insert text here.",
+  },
+  {
+    insert: "\n\n",
+  },
+  {
+    insert: "\n\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#000000",
+    },
+    insert: "PROJECT",
+  },
+  {
+    attributes: {
+      header: 1,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Project name – ",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      italic: true,
+    },
+    insert: "Detail",
+  },
+  {
+    attributes: {
+      header: 2,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert: "Insert text here Insert text here Insert text here.",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    insert: "\n\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#000000",
+    },
+    insert: "SKILLS",
+  },
+  {
+    attributes: {
+      header: 1,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Insert text here.",
+  },
+  {
+    attributes: {
+      list: "bullet",
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Insert text here.",
+  },
+  {
+    attributes: {
+      list: "bullet",
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Insert text here Insert text here Insert text here.",
+  },
+  {
+    attributes: {
+      list: "bullet",
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Insert text here Insert text here Insert text here.",
+  },
+  {
+    attributes: {
+      list: "bullet",
+    },
+    insert: "\n",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    insert: "\n\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#000000",
+    },
+    insert: "AWARDS",
+  },
+  {
+    attributes: {
+      header: 1,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+      bold: true,
+    },
+    insert: "Insert text here ",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert:
+      "Insert text here Insert text here Insert text here Insert text here",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+      bold: true,
+    },
+    insert: "Insert text here ",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert: "Insert text here Insert text here Insert text here.",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+      bold: true,
+    },
+    insert: "Insert text here",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert:
+      " Insert text here Insert text here Insert text here Insert text here",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+      bold: true,
+    },
+    insert: "Insert text here ",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert: "Insert text here Insert text here Insert text here.",
+  },
+  {
+    insert: "\n\n",
+  },
+  {
+    insert: "\n\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#000000",
+    },
+    insert: "LANGUAGE",
+  },
+  {
+    attributes: {
+      header: 1,
+    },
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#666666",
+    },
+    insert: "Insert text here Insert text here Insert text here ",
+  },
+  {
+    insert: "\n\n",
+  },
+];
+
+export const letterTemplate = [
+  {
+    attributes: {
+      background: "transparent",
+      color: "#00000",
+      bold: true,
+    },
+    insert: "Your Name",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      italic: true,
+    },
+    insert: "123 Your Street",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      italic: true,
+    },
+    insert: "Your City, ST 12345",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "(123) 456-7890",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "no_reply@example.com",
+  },
+  {
+    insert: "\n\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "4th September 20XX",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Ronny Reader",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "CEO, Company Name",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "123 Address St ",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert: "Anytown, ST 12345",
+  },
+  {
+    insert: "\n\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      bold: true,
+      italic: true,
+    },
+    insert: "Dear Ms. Reader,",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert:
+      "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan.",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+    },
+    insert:
+      "Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius.",
+  },
+  {
+    insert: "\n\n\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      italic: true,
+      bold: true,
+    },
+    insert: "Sincerely,",
+  },
+  {
+    insert: "\n",
+  },
+  {
+    attributes: {
+      background: "transparent",
+      color: "#00ab44",
+      bold: true,
+    },
+    insert: "Your Name",
+  },
+  {
+    insert: "\n\n\n",
+  },
+];
